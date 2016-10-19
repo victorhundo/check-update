a2e0f1b6dfcfd6eefa6b9aa8883086e5919de676
victor.sousa <vhfsfox@gmail.com>
2016-10-19 14:13:17 -0300

    Correção de erro com Cors

--- app/index.js
+++ app/index.js
@@ -7,6 +7,14 @@ require('shelljs/global');
 const server_port = 8042;
 const app = express();
 
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	//req.setHeader('Authorization');
+	next();
+});
+
 app.get('/', function (req, res) {
 	saida = [];
     exec('cp script/out containers-list', {silent:true});
